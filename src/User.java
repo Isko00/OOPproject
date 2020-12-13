b9196ffecd2d8255bd8ba5dcaa3b9497634b1a4c
Anguis <Islam.ospanov00@gmail.com>
2020-12-13 19:14:54 +0600

    User, UserInfo classes created.

---
+++ src/User.java
@@ -0,0 +1,59 @@
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5768743020744465843L;
+	private final int id;
+	private static int lastId = 0;
+	private String password;
+	private UserInfo info;
+
+	public User(String password, UserInfo info) {
+		this.setPassword(password);
+		this.info = info;
+		id = lastId++;
+	}
+	
+	public User(String password) {
+		this(password, new UserInfo());
+	}
+	
+	public User() {
+		this("");
+	}
+	
+	public int getId() { return id; }
+	
+	public UserInfo getInfo() { return info; }
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setInfo(UserInfo info) { this.info = info; }
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int hashCode() { return id; }
+	
+	public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        User u = (User) o;
+        return hashCode() == u.hashCode();
+	}
+	
+	public String toString() {
+		return "id [" + id + "]\n"
+				+ "info [" + info.toString() + "]";
+	}
+}
---
+++ src/UserInfo.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6845130151436394015L;
+	private HashMap<String, String> properties;
+	
+	public UserInfo() {
+		properties = new HashMap<String, String>();
+	}
+	
+	public void setProperty(String property, String value) {
+		properties.putIfAbsent(property, value);
+	}
+	
+	public String getProperty(String property) {
+		return properties.get(property);
+	}
+	
+	public HashMap<String, String> getAllProperties() {
+		return properties;
+	}
+	
+	public int hashCode() {
+		return properties.hashCode();
+	}
+	
+	public boolean equals(Object o) {
+		return properties.equals(o);
+	}
+	
+	public String toString() {
+		String result = "";
+		
+		for (HashMap.Entry<String, String> entry : properties.entrySet()) {
+		    String key = entry.getKey();
+		    String value = entry.getValue();
+		    result += key + " [" + value + "]\n";
+		}
+
+		return result;
+	}
+}
