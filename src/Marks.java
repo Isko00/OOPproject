7a002c79a34235a5dd2b2932338965e82174f113
Anguis <Islam.ospanov00@gmail.com>
2020-12-13 20:08:02 +0600

    Marks class created.

---
+++ src/Marks.java
@@ -0,0 +1,56 @@
+public class Marks {
+	/* First certification = marks[0]
+	 * Second certification = marks[1]
+	 * Final exam = marks[2]
+	 */
+	public int[] marks;
+
+	public Marks () {
+		marks = new int[3];
+	}
+	
+	public int hashCode() {
+	    return marks[0] * 100
+	    		+ marks[1] * 10
+	    		+ marks[2];
+	}
+
+	public boolean equals(Object o) {
+		if ((this != o) 
+				|| this == null
+				|| this.getClass() != o.getClass()) {
+			return false;
+		}
+		Marks mark = (Marks)o;
+		return this.hashCode() == mark.hashCode();
+	}
+
+	public String toString() {
+		return "First certification [" + marks[0] + "]\n"
+				+ "Second certification [" + marks[1] + "]\n"
+				+ "Final exam [" + marks[2] + "]";
+	}
+
+	public void setMarks(int[] marks) {
+		this.marks = marks;
+	}
+
+	public int[] getMarks() { return marks; }
+
+	public String getMarkChar(int mark) {
+		if (100 >= mark && mark >= 95) return "A";
+		else if (94 >= mark && mark >= 90) return "A-";
+		else if (89 >= mark && mark >= 85) return "B+";
+		else if (84 >= mark && mark >= 80) return "B";
+		else if (79 >= mark && mark >= 75) return "B-";
+		else if (74 >= mark && mark >= 70) return "C+";
+		else if (69 >= mark && mark >= 65) return "C";
+		else if (64 >= mark && mark >= 60) return "C-";
+		else if (60 >= mark && mark >= 50) return "D";
+		else return "F";
+	}
+
+	public double getGPA() {
+	    return 3.00;
+	}
+}
